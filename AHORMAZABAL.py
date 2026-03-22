6a64867f589fdc7478c38c67a98e2a2a46279b4d
Andy Hormazabal <andy@andy.cl>
2026-03-22 02:39:03 -0300

    feat: add jenkins

--- AHORMAZABAL.py
+++ AHORMAZABAL.py
@@ -1,5 +1,11 @@
+nombre = "Andy"
+numero1 = 6
+numero2 = 7
+resultado = numero1 * numero2
+
+
 def render_page():
-    return """
+    return f"""
     <html>
       <head>
         <meta charset="utf-8" />
@@ -8,6 +14,8 @@ def render_page():
       <body style="font-family: Arial, sans-serif; margin: 40px;">
         <h1>Archivo AHORMAZABAL.py</h1>
         <p>Esta ruta se esta ejecutando correctamente.</p>
+        <p>Nombre: {nombre}</p>
+        <p>Resultado (numero1 * numero2): {resultado}</p>
         <a href="/">Volver al index</a>
       </body>
     </html>
