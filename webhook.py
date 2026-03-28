2e405a49e3f3dd6eee73ca87c827e704431e43a6
Andy Hormazabal <andy@andy.cl>
2026-03-28 02:24:32 -0300

    feat: add ruta chatboot  liquidacion sueldo con email

--- webhook.py
+++ webhook.py
@@ -2,14 +2,14 @@ import os
 import requests
 from flask import request, jsonify
 
-MAILGUN_API_KEY = os.getenv("mailgun_api_key", "")
+mak = os.getenv("mak", "")
 MAILGUN_DOMAIN = "sandbox63cc9589838a4c69918c906ea969ece5.mailgun.org"
 
 
 def enviar_email(destinatario, number):
     return requests.post(
         f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
-        auth=("api", MAILGUN_API_KEY),
+        auth=("api", mak),
         data={
             "from": f"Liquidaciones <postmaster@{MAILGUN_DOMAIN}>",
             "to": destinatario,
