8f2504b80c657a2a2044ae4e005195d5ce2c62ca
Andy Hormazabal <andy@andy.cl>
2026-03-28 02:34:50 -0300

    feat: add ruta chatboot  liquidacion sueldo con email

--- index.py
+++ index.py
@@ -5,7 +5,6 @@ from flask import Flask, request
 
 from ejemplos.db import get_db_connection
 from ejemplos.guardar_usuario import registrar_rutas as registrar_rutas_guardar_usuario
-from webhook import registrar_rutas as registrar_rutas_webhook
 
 app = Flask(__name__)
 
@@ -321,7 +320,6 @@ for module_name in MODULE_NAMES:
     )
 
 registrar_rutas_guardar_usuario(app)
-registrar_rutas_webhook(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
