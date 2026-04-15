4cefc90e9c51c63c6091d7026a6a35c59151b9c8
Zrain <zebba.leniz@gmail.com>
2026-04-14 21:52:37 -0400

    python

--- PRACTICAS/02_variables.py
+++ PRACTICAS/02_variables.py
@@ -5,9 +5,14 @@ nombre = "Ana"     # Texto (string)
 edad = 20          # Numero entero (int)
 altura = 1.65      # Numero decimal (float)
 
+peso = 70          # Peso en kg
+imc = peso / (altura ** 2)  # Calcula el indice de masa corporal
+
 print("Nombre:", nombre)
 print("Edad:", edad)
 print("Altura:", altura)
+print("Peso:", peso)
+print("IMC:", imc)
 
 # Tambien puedes cambiar el valor de una variable.
 edad = 21
--- SRAIN.py
+++ SRAIN.py
@@ -1,9 +1,15 @@
 def render_page():
+    altura = 1.65      # Numero decimal (float)
+    peso = 70          # Peso en kg
+    imc = peso / (altura ** 2)  # Calcula el indice de masa corporal
+
     return """
     <html>
       <head>
         <meta charset="utf-8" />
-        <title>SRAIN.py</title>
+        <title>ZRAIN.py</title>
+        <p>> IMC: {imc}</p>
+
       </head>
       <body style="font-family: Arial, sans-serif; margin: 70px;">
         <h1>Archivo SRAIN.py</h1>
