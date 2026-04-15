b08f2fe5de9ea16ef933c163ad26c430ac9bcac7
antony <acruzadovela@gmail.com>
2026-04-14 22:11:11 -0400

    update

--- JCRUZADO.py
+++ JCRUZADO.py
@@ -9,14 +9,14 @@ def render_page():
     <html>
       <head>
         <meta charset="utf-8" />
-        <title>CTRONCOSO.py</title>
+        <title>JCRUZADO.py</title>
       </head>
       <body style="font-family: Arial, sans-serif; margin: 40px;">
 
-        <h1>Archivo CTRONCOSO.py</h1>
+        <h1>Archivo JCRUZADO.py</h1>
         <p>IMC: {imc:.2f} *****</p>
         <p>Esta ruta se esta ejecutando correctamente.</p>
         <a href="/">Volver al index</a>
       </body>
     </html>
-    """
+    f"""
