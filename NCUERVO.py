85e065b7bdd1002337195087b8451ad7d0f06c01
norbey <norbey@gmail.com>
2026-04-14 22:04:52 -0400

    Agrego texto hola de nuevo en norbey.txt

--- NCUERVO.py
+++ NCUERVO.py
@@ -1,14 +1,24 @@
 def render_page():
-    return """
+    nombre = "Nicolas"
+    edad = 20
+    activo = True
+
+    peso = 70
+    altura = 1.75
+    imc = peso / (altura ** 2)
+
+    return f"""
     <html>
       <head>
         <meta charset="utf-8" />
         <title>NCUERVO.py</title>
       </head>
-      <body style="font-family: Arial, sans-serif; margin: 40px;">
+      <body style="font-family: Arial; margin: 40px;">
         <h1>Archivo NCUERVO.py</h1>
-        <p>Esta ruta se esta ejecutando correctamente.</p>
-        <a href="/">Volver al index</a>
+        <p>Nombre: {nombre}</p>
+        <p>Edad: {edad}</p>
+        <p>Activo: {activo}</p>
+        <p>IMC: {imc:.2f}</p>
       </body>
     </html>
     """
