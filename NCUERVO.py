262b5ffcec2b4ca0616c1d8bcb8bf34b1f2ad6dd
norbey <norbey@gmail.com>
2026-04-23 14:01:36 -0400

    Actualizacion de rutas en index y archivos de alumnos

--- NCUERVO.py
+++ NCUERVO.py
@@ -1,13 +1,21 @@
 def render_page():
-    return """
+    # DATOS
+    nombre = "NORBEY"
+    peso = 70
+    estatura = 1.70
+    
+    # CÁLCULO
+    imc = peso / (estatura * estatura)
+    imc_redondeado = round(imc, 2)
+
+    # HTML PURO (SIN ESTILOS)
+    return f"""
     <html>
-      <head>
-        <meta charset="utf-8" />
-        <title>NCUERVO.py</title>
-      </head>
-      <body style="font-family: Arial, sans-serif; margin: 40px;">
-        <h1>Archivo NCUERVO.py</h1>
+      <body>
+        <p>> IMC: {imc_redondeado}</p>
+        <h1>Archivo {nombre}.py</h1>
         <p>Esta ruta se esta ejecutando correctamente.</p>
+        <br>
         <a href="/">Volver al index</a>
       </body>
     </html>
--- index.py
+++ index.py
@@ -24,6 +24,7 @@ MODULE_NAMES = [
     "STAPIA",
     "CTRONCOSO",
     "RZAMORANO",
+    "NORBEY", # <-- AGREGA ESTA LÍNEA AQUÍ
 ]
 
 MYSQL_LINKS = [
